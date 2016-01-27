226bab122daff00f733edfaf3ca81d6930fa15df
Vincent Simon <vincent.simon@gadz.org>
2016-01-27 19:11:19 +0100

    ca avance bien

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,9 @@
 class Video < ActiveRecord::Base
   include AlgoliaSearch
 
+  CATEGORY = ["Développement du dirigeant", "Environnement de l'entreprise", "Management", "Technique d'entreprise"]
+  TAG = ["Parole d'expert", "Extraclub", "Parole d'adhérent", "Maison APM", "Convention"]
+
   algoliasearch do
     attributesToIndex ['title', 'description', 'expert', 'tag', 'category', 'subcategory']
 
