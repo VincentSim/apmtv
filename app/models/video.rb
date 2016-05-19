fb94774c4e8245f6af53084f8b97c949ce3a2449
Vincent Simon <vincent.simon@gadz.org>
2016-05-19 10:09:18 +0200

    categories updated

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,7 +1,7 @@
 class Video < ActiveRecord::Base
   include AlgoliaSearch
 
-  CATEGORY = ["Développement du dirigeant", "Environnement de l'entreprise", "Management", "Technique d'entreprise"]
+  CATEGORY = ["Développement du dirigeant", "Environnement de l'entreprise", "Management", "Technique d'entreprise", "Tutos", "Autres"]
   TAG = ["Parole d'expert", "Extraclub", "Parole d'adhérent", "Maison APM", "Convention"]
 
   algoliasearch per_environment: true do
